0c02f20d2c7bf8ab0ed810e8fda91d0589050b66
alex meza <alex.meza.5471@gmail.com>
2025-04-09 15:11:24 -0600

    Se crean los archivos index.html, welcome.html y script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+//ACCION DEL BOTON GUARDAR...RECUPERA INFORMACION DEL USUARIO Y LA AÑADE AL LOCALSOTAGE
+function recuperarInformacion(){
+    let nombreUsuario = document.getElementById("nombre").value;
+    localStorage.setItem("usuario",nombreUsuario);
+}
+
+//ACCION DEL BOTON ELIMINAR... ELIMINA LA INFORMACION DEL LOCALSTORAGE...
+function eliminarInformacion(){
+    localStorage.removeItem("usuario");
+}
+
+
+//VALIDACION DEL LOCALSTORAGE...
+let placeholder = document.getElementById("placeholder-usuario");
+
+if(localStorage.getItem("usuario") != null){
+    
+    let nombre = localStorage.getItem("usuario");
+    placeholder.innerText = `Hola, ${nombre}, bienvenida/o de nuevo.`;
+    
+    if(nombre === ""){
+        placeholder.innerText = `Por favor ve a index e ingresa tu nombre`; 
+    }
+    
+    
+}
+else{
+   
+    placeholder.innerText = `Por favor ve a index e ingresa tu nombre`;  
+}
+
+
+
+
+
+
+
